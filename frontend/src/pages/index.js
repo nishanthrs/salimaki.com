debf266d6baa6409e940b189474775fd831b53fd
Nishanth Salinamakki <nrs3@illinois.edu>
2021-04-18 16:45:46 -0700

    Added blog boilerplate for NextJS+Strapi

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,90 +1,90 @@
-// import React from "react";
-// import Articles from "../components/articles";
-// import Layout from "../components/layout";
-// import Seo from "../components/seo";
-// import { fetchAPI } from "../lib/api";
+import React from "react";
+import Articles from "../components/Articles";
+import Layout from "../components/Layout";
+import Seo from "../components/Seo";
+import { fetchAPI } from "../lib/api";
 
-// const Home = ({ articles, categories, homepage }) => {
-//   return (
-//     <Layout categories={categories}>
-//       <Seo seo={homepage.seo} />
-//       <div className="uk-section">
-//         <div className="uk-container uk-container-large">
-//           <h1>{homepage.hero.title}</h1>
-//           <Articles articles={articles} />
-//         </div>
-//       </div>
-//     </Layout>
-//   );
-// };
+const Home = ({ articles, categories, homepage }) => {
+  return (
+    <Layout categories={categories}>
+      <Seo seo={homepage.seo} />
+      <div className="uk-section">
+        <div className="uk-container uk-container-large">
+          <h1>{homepage.hero.title}</h1>
+          <Articles articles={articles} />
+        </div>
+      </div>
+    </Layout>
+  );
+};
 
-// export async function getStaticProps() {
-//   // Run API calls in parallel
-//   const [articles, categories, homepage] = await Promise.all([
-//     fetchAPI("/articles?status=published"),
-//     fetchAPI("/categories"),
-//     fetchAPI("/homepage"),
-//   ]);
+export async function getStaticProps() {
+  // Run API calls in parallel
+  const [articles, categories, homepage] = await Promise.all([
+    fetchAPI("/articles?status=published"),
+    fetchAPI("/categories"),
+    fetchAPI("/homepage"),
+  ]);
 
-//   return {
-//     props: { articles, categories, homepage },
-//     revalidate: 1,
-//   };
-// }
+  return {
+    props: { articles, categories, homepage },
+    revalidate: 1,
+  };
+}
 
-// export default Home;
+export default Home;
 
-import {
-  Link as ChakraLink,
-  Text,
-  Code,
-  List,
-  ListIcon,
-  ListItem,
-} from '@chakra-ui/react'
-import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
-import { Hero } from '../components/Hero'
-import { Container } from '../components/Container'
-import { Main } from '../components/Main'
-import { DarkModeSwitch } from '../components/DarkModeSwitch'
-import { CTA } from '../components/CTA'
-import { Footer } from '../components/Footer'
+// import {
+//   Link as ChakraLink,
+//   Text,
+//   Code,
+//   List,
+//   ListIcon,
+//   ListItem,
+// } from '@chakra-ui/react'
+// import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
+// import { Hero } from '../components/Hero'
+// import { Container } from '../components/Container'
+// import { Main } from '../components/Main'
+// import { DarkModeSwitch } from '../components/DarkModeSwitch'
+// import { CTA } from '../components/CTA'
+// import { Footer } from '../components/Footer'
 
-const Index = () => (
-  <Container height="100vh">
-    <Hero />
-    <Main>
-      <Text>
-        Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code>.
-      </Text>
+// const Index = () => (
+//   <Container height="100vh">
+//     <Hero />
+//     <Main>
+//       <Text>
+//         Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code>.
+//       </Text>
 
-      <List spacing={3} my={0}>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink
-            isExternal
-            href="https://chakra-ui.com"
-            flexGrow={1}
-            mr={2}
-          >
-            Chakra UI <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink isExternal href="https://nextjs.org" flexGrow={1} mr={2}>
-            Next.js <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-      </List>
-    </Main>
+//       <List spacing={3} my={0}>
+//         <ListItem>
+//           <ListIcon as={CheckCircleIcon} color="green.500" />
+//           <ChakraLink
+//             isExternal
+//             href="https://chakra-ui.com"
+//             flexGrow={1}
+//             mr={2}
+//           >
+//             Chakra UI <LinkIcon />
+//           </ChakraLink>
+//         </ListItem>
+//         <ListItem>
+//           <ListIcon as={CheckCircleIcon} color="green.500" />
+//           <ChakraLink isExternal href="https://nextjs.org" flexGrow={1} mr={2}>
+//             Next.js <LinkIcon />
+//           </ChakraLink>
+//         </ListItem>
+//       </List>
+//     </Main>
 
-    <DarkModeSwitch />
-    <Footer>
-      <Text>Next ❤️ Chakra</Text>
-    </Footer>
-    <CTA />
-  </Container>
-)
+//     <DarkModeSwitch />
+//     <Footer>
+//       <Text>Next ❤️ Chakra</Text>
+//     </Footer>
+//     <CTA />
+//   </Container>
+// )
 
-export default Index;
+// export default Index;
